a9b896412a0dcda6aae9c8d431db69106be1956a
ALHASAN  ALSHAEBI <lahdan01@ads.uni-passau.de>
2022-02-08 19:21:43 +0100

    Update inference.py

--- inference.py
+++ inference.py
@@ -2,6 +2,7 @@ import os
 import torch
 import numpy as np
 import cv2
+from efficientDet import EfficientDet
 colors = [(39, 129, 113), (164, 80, 133), (83, 122, 114), (99, 81, 172), (95, 56, 104), (37, 84, 86), (14, 89, 122),
           (80, 7, 65), (10, 102, 25), (90, 185, 109), (106, 110, 132), (169, 158, 85), (188, 185, 26)]
 
