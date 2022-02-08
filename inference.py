0adfab387e29755f8f4c84e43428054f07b01077
ALHASAN  ALSHAEBI <lahdan01@ads.uni-passau.de>
2022-02-08 19:31:59 +0100

    Update inference.py

--- inference.py
+++ inference.py
@@ -3,6 +3,11 @@ import torch
 import numpy as np
 import cv2
 from efficientDet import EfficientDet
+from torchvision import transforms
+from load_DS import CocoDataset, collater
+from scaling import Resizer
+from norm import Normalizer
+from torch.utils.data import DataLoader
 colors = [(39, 129, 113), (164, 80, 133), (83, 122, 114), (99, 81, 172), (95, 56, 104), (37, 84, 86), (14, 89, 122),
           (80, 7, 65), (10, 102, 25), (90, 185, 109), (106, 110, 132), (169, 158, 85), (188, 185, 26)]
 
